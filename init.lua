f1bef8cec8830a87e92f6e0fdad2f1f5b6c4cc55
Sam <sprtlw@pm.me>
2026-01-12 03:14:55 -0500

    augroup for CursorLine; prevent new single line comment on enter

--- init.lua
+++ init.lua
@@ -235,6 +235,15 @@ vim.keymap.set('n', '<C-S-k>', '<C-w>K', { desc = 'Move window to the upper' })
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+-- Disable auto-continuation of comments on Enter
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('kickstart-disable-comment-continuation', { clear = true }),
+  pattern = '*',
+  callback = function()
+    vim.opt_local.formatoptions:remove { 'r', 'o' }
+  end,
+})
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
@@ -248,6 +257,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- Keep CursorLine background transparent for all themes
 vim.api.nvim_create_autocmd('ColorScheme', {
+  group = vim.api.nvim_create_augroup('kickstart-colorscheme', { clear = true }),
   callback = function()
     vim.api.nvim_set_hl(0, 'CursorLine', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'CursorLineNr', { bold = true })
