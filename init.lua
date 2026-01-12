a11ee568705dab40873282b11135e2b1f5c4ea37
Sam <sprtlw@pm.me>
2026-01-11 23:11:02 -0500

    Change preset to enter for completion, as well as tab and shift tab cycling; rust blink implementation; remove rustowl; enable inlay hints for rust by default

--- init.lua
+++ init.lua
@@ -644,6 +644,10 @@ require('lazy').setup({
           --
           -- This may be unwanted, since they displace some of your code
           if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+            -- Enable inlay hints by default
+            if vim.bo[event.buf].filetype == 'rust' then
+              vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
+            end
             map('<leader>th', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
@@ -869,13 +873,15 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        preset = 'enter',
 
         -- Disable C-k keybind
         ['<C-k>'] = { 'fallback' },
 
         -- Rebind Previous Functionality
         ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'fallback' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -914,7 +920,7 @@ require('lazy').setup({
       -- the rust implementation via `'prefer_rust_with_warning'`
       --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,13 +4,6 @@
 -- See the kickstart.nvim README for more information
 return {
   {
-    'cordx56/rustowl',
-    version = '*', -- Latest stable version
-    build = 'cargo install rustowl',
-    lazy = false, -- This plugin is already lazy
-    opts = {},
-  },
-  {
     'mrcjkb/rustaceanvim',
     version = '^6', -- Recommended
     lazy = false, -- This plugin is already lazy
